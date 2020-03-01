3839c9e161f8fcb04c23e9d91665b6096a6d1f14
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2020-03-01 16:28:56 +0200

    Use github token to validate user access token

--- src/auth.ts
+++ src/auth.ts
@@ -1,33 +1,35 @@
 import passport from 'passport';
-import { Strategy as GitHubStrategy } from 'passport-github2';
+import GitHubTokenStrategy from 'passport-github-token';
 import { Request, Response } from 'express';
+import { pathOr, compose, trim, split, last } from 'ramda';
 
-const APP_ORIGIN = process.env.APP_ORIGIN;
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
-const GITHUB_CALLBACK_URL = process.env.GITHUB_CALLBACK_URL;
+
+// get authroziation from headers
+const authorization = compose(
+  trim,
+  last,
+  split(' '),
+  pathOr('', ['headers', 'authorization']),
+);
 
 const authenticate = () => {
   const githubTokenStrategyCallback = (
     accessToken: string,
     refreshToken: string,
     profile: string,
-    done: (
-      err: null,
-      value: { accessToken: string, refreshToken: string, profile: string },
-    ) => unknown,
-  ): unknown =>
-    done(null, {
-      accessToken,
-      refreshToken,
-      profile,
-    });
+    next,
+  ): unknown => {
+    // console.log(profile, 'profile');
+    next(null, profile);
+  };
 
-  const strategy = new GitHubStrategy(
+  const strategy = new GitHubTokenStrategy(
     {
       clientID: GITHUB_CLIENT_ID,
       clientSecret: GITHUB_CLIENT_SECRET,
-      callbackURL: `${APP_ORIGIN}/${GITHUB_CALLBACK_URL}`,
+      // callbackURL: `${APP_ORIGIN}/${GITHUB_CALLBACK_URL}`,
     },
     githubTokenStrategyCallback,
   );
@@ -38,17 +40,21 @@ const authenticate = () => {
     initialize: () => {
       return passport.initialize();
     },
-    authenticateGithub: (req: Request, res: Response): Promise<object> =>
-      new Promise((resolve, reject) => {
+    authenticateGithub: (req: Request, res: Response): Promise<object> => {
+      //mutate
+      req.query.access_token = authorization(req);
+
+      return new Promise((resolve, reject) => {
         passport.authenticate(
-          'google-token',
+          'github-token',
           { session: false },
           (err: any, data: any, info: any) => {
             if (err) reject(err);
             resolve({ data, info });
           },
         )(req, res);
-      }),
+      });
+    },
   };
 };
 
